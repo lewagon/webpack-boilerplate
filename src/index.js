33736abc3081ef3d2b7011ed172d8d1d39ae32da
Damien Milon <37899667+dmilon@users.noreply.github.com>
2022-05-31 14:00:21 +0200

    add babel-plugin-transform-runtime

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log("Hello from src/index.js!");
+const fetchApi = async () => {
+  const response = await fetch("http://www.omdbapi.com/?s=harry potter&apikey=adf1f2d7");
+  const data = await response.json();
+  console.log(data);
+};
+
+fetchApi();
