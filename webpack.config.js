023ea143140e77de3eaaa064655add575d4c8b76
Hafid Qarchi <haqarchi@gmail.com>
2022-12-12 23:01:33 +0900

    html webpack plugin added

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
   mode: "development",
+  entry: path.resolve(__dirname, "src/index.js"),
+
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "main.js"
+    filename: "main.js",
   },
   devtool: "source-map",
   module: {
@@ -13,8 +14,8 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
-      }
-    ]
-  }
+        loader: "babel-loader",
+      },
+    ],
+  },
 };
