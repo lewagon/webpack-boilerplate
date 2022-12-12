08c45eb986f7b08967f21c9376112be5bb73f331
Hafid Qarchi <haqarchi@gmail.com>
2022-12-12 23:36:32 +0900

    entry point changed to hash incase multiple entry required and cache config for main js defined

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,12 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: path.resolve(__dirname, "src/index.js"),
+  entry: {
+    main: path.resolve(__dirname, "src/index.js"),
+  },
 
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
+    filename: "[name][contenthash].js",
   },
   devtool: "source-map",
   module: {
