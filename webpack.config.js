fc168a7abb54b99a605cd934f6c56d8d36bdb79b
Hafid Qarchi <haqarchi@gmail.com>
2022-12-12 23:21:05 +0900

    html webpack plugin config defined

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -18,4 +19,11 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack App",
+      filename: "main.html",
+      template: "index.html",
+    }),
+  ],
 };
