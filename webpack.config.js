082b2771bf2539ff03c77a5cdf72ccc665459fb4
Hafid Qarchi <haqarchi@gmail.com>
2022-12-13 00:35:44 +0900

    changed the plugins filename and template to be the same in order for root to point to index.html page

--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,22 @@ module.exports = {
   entry: {
     main: path.resolve(__dirname, "src/index.js"),
   },
-
   output: {
+    path: path.resolve(__dirname, "dist"),
     filename: "[name][contenthash].js",
+    clean: true,
+    // assetModuleFilename: "[name][ext]",
   },
   devtool: "source-map",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    open: true,
+    hot: true,
+    compress: true,
+    // historyApiFallback: true,
+  },
   module: {
     rules: [
       {
@@ -23,7 +34,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: "Webpack App",
-      filename: "main.html",
+      filename: "index.html",
       template: "index.html",
     }),
   ],
